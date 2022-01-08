569a83535c3ba4864105dc243284fd8e5917ff61
Startemy <93431881+Startemy@users.noreply.github.com>
2022-01-08 10:49:48 +0300

    Homework (#1)
    * ДЗ_1 pullreq
    
    * ДЗ_1 pullreq
    
    * Сделал кнопочками для эксперимента
    
    * добавил дз-2
    
    * добавил описание к 4 заданию
    
    * ДЗ №2
    
    * ДЗ №2 подправил выводы в консоль вместо alert
    
    * ДЗ-3 задания 1-3
    
    * ДЗ-3 задание 4 доделать
    
    * ДЗ-3 задание 4 сделал на половину
    
    * ДЗ-3 подправил 4-е задание
    
    * ДЗ-3 готово
    
    * ДЗ-2 добавил в 3 задание - если числа одинаковые
    
    * ДЗ-4 задание 1 - 1.2
    
    * ДЗ-4 без допов
    
    * ДЗ-5 заданя 1.1 - 1.3
    
    * ДЗ-5 заданя 1.4 и 2.1
    
    * ДЗ-5 заданя 1.4 и 2.1 исправил косяки
    
    * ДЗ-5 готово
    
    * ДЗ-5 написал click по другому
    
    * ДЗ-6 задания 1 уровня
    
    * ДЗ-6 задиния 2
    
    * ДЗ-2 перечитал задание 2-5 и переделал
    
    * ДЗ-6 задание 3-1
    
    * ДЗ-6 задиние 3 доделать
    
    * прошелся по всем домашкам еще раз + дз-6 3-3
    
    * дз-6 задание 3-5 доделать
    
    * ДЗ-6 задание 3-5
    
    * Работа над ошибками №1
    
    * работа над ошибками + rating-previousElementSibling не получается
    
    * работа над ошибками 1,2,3,5
    
    * Разделил 1-4 дз

---
+++ homework4.js
@@ -0,0 +1,98 @@
+'use strict';
+
+/////ДЗ-4////////////
+//4-1 Написать функцию, преобразующую число в объект
+function numtoobj() {
+    let num = Number(prompt('Введите число от 0 до 999'));
+    num = Math.floor(num);
+    if (num < 0 || num > 999 || !Number.isInteger(num)) {
+        alert('Вы ввели не то число или вовсе не число');
+    }
+    class Propofnum {
+        constructor(units, tens, hundreds) {
+            this.units = units;
+            this.tens = tens;
+            this.hundreds = hundreds;
+        }
+    }
+    let obj = new Propofnum(num % 10, Math.floor(num / 10) % 10, Math.floor(num / 100))
+    console.log(obj);
+}
+
+//4-1-1
+function prod() {
+    ////es5
+    function Product(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    Product.prototype.make25PercentDiscount = function () {
+        this.price = (this.price - (this.price * 25 / 100));
+    }
+    let product = new Product('Milanchiki', 45);
+    product.make25PercentDiscount();
+    console.log(product);
+
+
+    //es6
+    class Product_scnd {
+        constructor(name, price) {
+            this.name = name;
+            this.price = price;
+        }
+        make25PercentDiscount() {
+            this.price = (this.price - (this.price * 25 / 100));
+        }
+    }
+    let product_ = new Product_scnd('Milanchiki', 45);
+    product.make25PercentDiscount();
+    console.log(product_);
+}
+
+//4-1-2
+function post() {
+    let push = prompt('Сообщение');
+    ///es5
+    function Post(author, text, date) {
+        this.author = author;
+        this.text = text;
+        this.date = date;
+    }
+    Post.prototype.medit = function () {
+        this.text = push;
+    }
+    function Attac(author, text, date) {
+        Post.call(this, author, text, date);
+        this.highlighted = false;
+    }
+    Attac.prototype = Object.create(Post.prototype);
+    Attac.prototype.constructor = Attac;
+    Attac.prototype.makeTextHighlighted = function () {
+        this.highlighted = true;
+    }
+
+    ///////es6
+    class Post_scnd {
+        constructor(author, text, date) {
+            this.author = author;
+            this.text = text;
+            this.date = date;
+        }
+        medit() {
+            this.text = push;
+        }
+    }
+    class Attac_scnd extends Post_scnd {
+        constructor(author, text, date) {
+            super(author, text, date);
+            this.highlighted = false;
+        }
+        makeTextHighlighted() {
+            this.highlighted = true;
+        }
+    }
+    let attac = new Attac_scnd('Artem', '', '26.12.21');
+    attac.makeTextHighlighted();
+    attac.medit();
+    console.log(attac);
+}
